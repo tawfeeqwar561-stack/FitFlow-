3eaa9ebaf6fa14d7dcac46168ac070da7b100636
tawfeeq imtiyaz war <tawfeeqwar561@gmail.com>
2026-03-11 23:01:29 +0530

    implement service layer.

--- backend/app/services/__init__.py
+++ backend/app/services/__init__.py
@@ -2,3 +2,5 @@ from app.services.auth_service import AuthService
 from app.services.user_service import UserService
 from app.services.workout_service import WorkoutService
 from app.services.ai_service import AIService
+from app.services.food_ai_service import FoodAIService
+from app.services.calorie_service import CalorieService
